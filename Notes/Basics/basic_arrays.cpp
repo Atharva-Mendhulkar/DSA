ed301dac95f82d6e93c838cfb902f3c3b21d3793
Atharva-mendhu <your.email@example.com>
2025-02-23 21:15:34 +0530

    did basic arrays

--- Notes/Basics/basic_arrays.cpp
+++ Notes/Basics/basic_arrays.cpp
@@ -9,16 +9,59 @@ int sum_arr(int arr[], int n){
 	return sum;
 }
 
-int main(){
-	int n;
-	cin>>n;
-	int arr[n];
-
-	for(int i=0; i<n; i++){
-		cin>>arr[i];
+int sum_odd_arr(int arr[], int n){
+	int odd=0;
+	for(int i =0; i<n;i++){
+		if(arr[i]%2==1){
+			odd+=arr[i];
+		}
 	}
+	return odd;
+}
 
-	int result=sum_arr(arr,n);
-	cout<<result;
-	return 0;
-}	
+void reverse(int arr[], int n) {
+    int arrr[n];
+    int k = 0;
+
+    for (int i = n - 1; i >= 0; i--) {
+        arrr[k] = arr[i];
+        k++;
+    }
+
+    // Printing the reversed array
+    for (int i = 0; i < n; i++) {
+        cout << arrr[i];
+        if (i != n - 1) cout << ",";
+    }
+    cout << endl;
+}
+
+bool issorted(int arr[], int n) {
+    bool sort = true;
+
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            sort = false;
+            break;
+        }
+    }
+    return sort;
+}
+
+int main() {
+    int n;
+    cin >> n;  // Size of array
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    if (issorted(arr, n)) {
+        cout << "Array is sorted in ascending order." << endl;
+    } else {
+        cout << "Array is NOT sorted in ascending order." << endl;
+    }
+
+    return 0;
+}
